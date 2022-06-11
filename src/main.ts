a241ba091fba8b7a3f303d5fb9d11cea7815b638
墨七 <meichangliang@mo7.cc>
2022-06-12 06:47:53 +0800

    exit-push

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import App from './App.svelte'
+declare const ViteConst: any;
 
+
+if (ViteConst) {
+  window.ViteConst = {
+    ...ViteConst,
+    rmAgin: 'mo777',
+  };
+}
+import { registerSW } from 'virtual:pwa-register';
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
+
+
+import App from '@/App.svelte'
+
+document.getElementById('SvelteApp').innerHTML=""
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('SvelteApp')
 })
 
 export default app  
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,21 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
+
+declare global {
+  interface Window {
+    mo7Encrypt: (msg: string) => string;
+    Md5: (msg: string) => string;
+    deferredPrompt: any;
+    ViteConst: {
+      AppVersion: string;
+      AppName: string;
+      ProxyUrl: string;
+      rmAgin: string;
+    };
+  }
+}
+
+
+export {};
