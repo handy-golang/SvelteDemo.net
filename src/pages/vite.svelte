626c6a61d064890275c8f1a62cce073ed056e62e
墨七 <meichangliang@mo7.cc>
2022-06-12 13:17:01 +0800

    exit-push

---
+++ src/pages/vite.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let count = 0;
+  const increment = (e: any) => {
+    count += 1;
+    console.info(`当前计数:${count}次`, e);
+  };
+</script>
+
+<div class="login">
+  vite 页面
+  <button on:click={increment}>
+    Clicks: {count}
+  </button>
+</div>
+
+<style lang="less">
+  .login {
+    background-color: red;
+  }
+</style>
