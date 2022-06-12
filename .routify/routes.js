3b4e6a9fc6d921ce20ad7af8fa17c2ce29defdbf
墨七 <meichangliang@mo7.cc>
2022-06-12 12:42:45 +0800

    exit-push

---
+++ .routify/routes.js
@@ -0,0 +1,42 @@
+
+/**
+ * @roxi/routify 2.18.7
+ * File generated Sun Jun 12 2022 12:40:33 GMT+0800 (China Standard Time)
+ */
+
+export const __version = "2.18.7"
+export const __timestamp = "2022-06-12T04:40:33.424Z"
+
+//buildRoutes
+import { buildClientTree } from "@roxi/routify/runtime/buildRoutes"
+
+//imports
+
+
+//options
+export const options = {}
+
+//tree
+export const _tree = {
+  "root": true,
+  "children": [
+    {
+      "isIndex": true,
+      "isPage": true,
+      "path": "/index",
+      "id": "_index",
+      "component": () => import('../src/pages/index.svelte').then(m => m.default)
+    },
+    {
+      "isPage": true,
+      "path": "/login",
+      "id": "_login",
+      "component": () => import('../src/pages/login.svelte').then(m => m.default)
+    }
+  ],
+  "path": "/"
+}
+
+
+export const {tree, routes} = buildClientTree(_tree)
+
