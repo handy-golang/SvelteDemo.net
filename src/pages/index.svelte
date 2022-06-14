d550f3b7a4e5649801cbeea5a434f50e6c2b1c31
墨七 <meichangliang@mo7.cc>
2022-06-14 18:29:40 +0800

    exit-push

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,19 +1,35 @@
 <script>
-  // https://github.com/sveltejs/kit/issues/981
-  export let scoped;
-  export let scopedSync;
+  import { getToken, mStorage } from '@/utils/tools';
+  import { outLogin } from '@/api/Account';
+  const token = getToken();
+  const UserInfo = mStorage.get('user_info');
+
+  const handleOutLogin = () => {
+    outLogin();
+  };
 </script>
 
 <div class="index">
-  首页
-  <div class="none">
-    {scoped}{scopedSync}
-    <slot />
-  </div>
+  {#if token}
+    <br />
+    <img class="Avatar" src={UserInfo.Avatar} alt="" srcset="" />
+    <br />
+    欢迎访问,{UserInfo.NickName}
+    <br />
+    <button on:click={handleOutLogin}>退出登录</button>
+  {/if}
+
+  {#if !token}
+    <br />
+    <a href="/login"> <button>请登录</button></a>
+    <br />
+  {/if}
 </div>
 
-<style>
-  .none {
-    display: none;
+<style lang="less">
+  .Avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 100px;
   }
 </style>
